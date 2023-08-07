b12771b2079537b12103a86fbf5e075eae8a7ab5
NixyJuppie <git.nixy.carefully356@passfwd.com>
2023-08-07 16:38:46 +0200

    Add AND/ORA/EOR

--- yanex-core/src/cpu/operation/operations/logic.rs
+++ yanex-core/src/cpu/operation/operations/logic.rs
@@ -33,3 +33,102 @@ impl BitTest {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub enum BitAnd {
+    Decoded(AddressingMode),
+    ReadingData(AddressingModeReadData),
+}
+
+impl BitAnd {
+    pub fn execute(&mut self, cpu: &mut Cpu, memory: &mut CpuMemory) -> Option<()> {
+        match self {
+            BitAnd::Decoded(mode) => {
+                let mut read = mode.begin_read_data();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator & data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+            BitAnd::ReadingData(read) => {
+                let mut read = read.clone();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator & data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum BitOr {
+    Decoded(AddressingMode),
+    ReadingData(AddressingModeReadData),
+}
+
+impl BitOr {
+    pub fn execute(&mut self, cpu: &mut Cpu, memory: &mut CpuMemory) -> Option<()> {
+        match self {
+            BitOr::Decoded(mode) => {
+                let mut read = mode.begin_read_data();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator | data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+            BitOr::ReadingData(read) => {
+                let mut read = read.clone();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator | data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum BitXor {
+    Decoded(AddressingMode),
+    ReadingData(AddressingModeReadData),
+}
+
+impl BitXor {
+    pub fn execute(&mut self, cpu: &mut Cpu, memory: &mut CpuMemory) -> Option<()> {
+        match self {
+            BitXor::Decoded(mode) => {
+                let mut read = mode.begin_read_data();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator ^ data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+            BitXor::ReadingData(read) => {
+                let mut read = read.clone();
+                let data = mem_read!(self, cpu, memory, read, ReadingData)?;
+
+                let result = cpu.registers.accumulator ^ data;
+                cpu.registers.accumulator = result;
+                cpu.registers.status.set_zero(result == 0);
+                cpu.registers.status.set_negative(result & 0b_1000_0000 > 0);
+                Some(())
+            }
+        }
+    }
+}
